e9165168d3236615d8d31831a32b6ad051cc93c9
abdulrahmanalshwish <abdulrahmanalshwish2022@gmail.com>
2024-05-07 12:03:45 +0300

    added hive packe to the project

--- lib/model/note_model.dart
+++ lib/model/note_model.dart
@@ -1,16 +1,22 @@
 class NoteModel {
   final String title;
   final String contents;
+  final String date;
+  final String color;
 
   NoteModel({
     required this.title,
     required this.contents,
+    required this.color,
+    required this.date,
   });
 
   factory NoteModel.fromJson(jsonData) {
     return NoteModel(
       title: jsonData['title'],
       contents: jsonData['content'],
+      date: jsonData['date'],
+      color: jsonData['color'],
     );
   }
 }
