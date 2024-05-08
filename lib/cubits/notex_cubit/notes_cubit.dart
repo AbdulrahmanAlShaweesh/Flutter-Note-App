8ab4448b23b1be91b603129d8f3c7e64ba5ea78d
abdulrahmanalshwish <abdulrahmanalshwish2022@gmail.com>
2024-05-08 17:58:38 +0300

    created note cubit to fetch the notes from Hive

--- lib/cubits/notex_cubit/notes_cubit.dart
+++ lib/cubits/notex_cubit/notes_cubit.dart
@@ -1,9 +1,26 @@
 import 'package:bloc/bloc.dart';
+import 'package:hive_flutter/hive_flutter.dart';
 import 'package:meta/meta.dart';
+import 'package:note_app/constants/constant_keys.dart';
 import 'package:note_app/model/note_model.dart';
 
 part 'notes_state.dart';
 
 class NotesCubit extends Cubit<NotesState> {
   NotesCubit() : super(NotesInitial());
+
+  fetchAllNotes() {
+    // since the data will come fast and no need of await, then we do not need to 
+    // emit loading state.
+    emit(NotesLoading());
+    try {
+      var noteBox = Hive.box<NoteModel>(kNoteBox);
+
+      List<NoteModel> notes = noteBox.values.toList();
+      // loads notes from hive box
+      emit(NotesSuccess(notes));
+    } catch (e) {
+      emit(NotesFailure(e.toString()));
+    }
+  }
 }
--- lib/views/home_screen.dart
+++ lib/views/home_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:note_app/cubits/notex_cubit/notes_cubit.dart';
 import 'package:note_app/widgets/add_note_show_sheet.dart';
 import 'package:note_app/widgets/custom_note_view_body.dart';
 
@@ -8,25 +10,28 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: const SafeArea(
-        child: CustomNoteViewBody(),
-      ),
-      floatingActionButton: FloatingActionButton(
-        backgroundColor: const Color(0xFF85fdde),
-        child: const Icon(
-          Icons.add,
-          color: Colors.black,
+    return BlocProvider(
+      create: (context) => NotesCubit(),
+      child: Scaffold(
+        body: const SafeArea(
+          child: CustomNoteViewBody(),
         ),
-        onPressed: () {
-          showModalBottomSheet(
-            isScrollControlled: true,
-            context: context,
-            builder: (context) {
-              return const AddNoteShowBottomSheet();
-            },
-          );
-        },
+        floatingActionButton: FloatingActionButton(
+          backgroundColor: const Color(0xFF85fdde),
+          child: const Icon(
+            Icons.add,
+            color: Colors.black,
+          ),
+          onPressed: () {
+            showModalBottomSheet(
+              isScrollControlled: true,
+              context: context,
+              builder: (context) {
+                return const AddNoteShowBottomSheet();
+              },
+            );
+          },
+        ),
       ),
     );
   }
